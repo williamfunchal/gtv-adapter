ef1551778b1094a6fdffd239e9c1f124b26bc032
William Funchal <william.funchal@consensus.com>
2023-08-23 12:26:26 -0300

    scheduler delay property added to turn scheduler configurable.

--- src/main/java/com/consensus/gtvadapter/poller/service/ISPDataService.java
+++ src/main/java/com/consensus/gtvadapter/poller/service/ISPDataService.java
@@ -1,6 +1,17 @@
 package com.consensus.gtvadapter.poller.service;
 
 
+import static com.consensus.common.sqs.CCSIQueueConstants.MessageAttributes.CORRELATION_ID;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
 import com.amazonaws.services.sqs.model.MessageAttributeValue;
 import com.consensus.common.util.CCSIUUIDUtils;
 import com.consensus.gtvadapter.common.models.dto.customer.IspS3CustomerDTO;
@@ -12,16 +23,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static com.consensus.common.sqs.CCSIQueueConstants.MessageAttributes.CORRELATION_ID;
 
 @Slf4j
 @Service
@@ -33,13 +34,12 @@ public class ISPDataService {
     private final ISPDataReadyMapper ispDataReadyMapper;
     private ObjectMapper objectMapper;
 
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(fixedDelayString = "${scheduler.delay}")
     public void fetchISPData() throws IOException, URISyntaxException{
         log.info("New ISP data found");
         
         //Read CSV Customer content from S3
         List<IspS3CustomerDTO> ispS3CustomerDTO = s3ReaderService.readCsvFromS3(IspS3CustomerDTO.class);
-        // List<IspS3CustomerDTO> ispS3CustomerDTO = s3ReaderService.readCsvFromLocalResource(IspS3CustomerDTO.class);
 
          for(IspS3CustomerDTO customerDTO : ispS3CustomerDTO){
             log.info("CustomerDTO: {}", customerDTO);
