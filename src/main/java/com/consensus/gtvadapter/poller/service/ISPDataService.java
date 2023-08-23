d8d7f1d757c856cbbb98e33cf191a64396c56c74
William Funchal <william.funchal@consensus.com>
2023-08-23 13:02:23 -0300

    using app.scheduler.isp-data-delay insted delay

--- src/main/java/com/consensus/gtvadapter/poller/service/ISPDataService.java
+++ src/main/java/com/consensus/gtvadapter/poller/service/ISPDataService.java
@@ -34,7 +34,7 @@ public class ISPDataService {
     private final ISPDataReadyMapper ispDataReadyMapper;
     private ObjectMapper objectMapper;
 
-    @Scheduled(fixedDelayString = "${scheduler.delay}")
+    @Scheduled(fixedDelayString = "${app.scheduler.isp-data-delay}")
     public void fetchISPData() throws IOException, URISyntaxException{
         log.info("New ISP data found");
         
