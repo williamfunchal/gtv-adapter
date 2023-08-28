08403e21614536961ddcc43ddc6e7fb6dd8f7576
William Funchal <williamfunchal@gmail.com>
2023-08-28 13:14:53 -0300

    CUP-34 Adding JSON type info to AddressBase (#17)
    - Adding JSON type info to AddressBase
    - Fixing mock url

--- src/main/java/com/consensus/gtvadapter/common/models/gtv/account/AddressBase.java
+++ src/main/java/com/consensus/gtvadapter/common/models/gtv/account/AddressBase.java
@@ -1,12 +1,23 @@
 package com.consensus.gtvadapter.common.models.gtv.account;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class AddressBase {
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "address_type",
+        visible = true)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = PostalAddress.class, name = "postal"),
+        @JsonSubTypes.Type(value = EmailAddress.class, name = "email")
+})
+public abstract class AddressBase {
 
     protected AddressType addressType;
     protected String purpose;
