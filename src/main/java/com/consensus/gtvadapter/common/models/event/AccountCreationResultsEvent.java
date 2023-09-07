0453cc211f687051812929aef0b2218e579e1243
William Funchal <williamfunchal@gmail.com>
2023-09-07 08:54:38 -0300

    CUP-38 Adding handler for 500 errors and WebClientRequestExceptions (#24)
    - Commenting code that use RDS connection

--- src/main/java/com/consensus/gtvadapter/common/models/event/AccountCreationResultsEvent.java
+++ src/main/java/com/consensus/gtvadapter/common/models/event/AccountCreationResultsEvent.java
@@ -1,22 +1,17 @@
 package com.consensus.gtvadapter.common.models.event;
 
 import com.consensus.gtvadapter.common.models.gtv.account.AccountCreationRequestBody;
-import com.consensus.gtvadapter.common.models.request.GtvRequestDetails;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
-import org.springframework.http.HttpMethod;
 
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class AccountCreationResultsEvent extends AdapterEvent {
+public class AccountCreationResultsEvent extends ResultsEvent {
 
     public static final String TYPE = "account-creation-result";
 
-    private HttpMethod method;
-    private String api;
     private AccountCreationRequestBody body;
-    private GtvRequestDetails result;
 
     public AccountCreationResultsEvent() {
         this.eventType = TYPE;
