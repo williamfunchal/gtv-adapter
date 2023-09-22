cdbe36c7ae74c6468592664710559c51e5b026db
William Funchal <williamfunchal@gmail.com>
2023-09-22 14:49:58 -0300

    CUP-29 Bumping parent pom version to get DNS resolver fix (#40)
    - Adding test payloads

--- src/main/java/com/consensus/gtvadapter/config/ObjectMapperConfig.java
+++ src/main/java/com/consensus/gtvadapter/config/ObjectMapperConfig.java
@@ -7,11 +7,13 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ObjectMapperConfig {
 
     @Bean
+    @Primary
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
                 .registerModule(new JavaTimeModule())
